407b3276ed9339f27d7ae220425e129be7221449
FajarWG <fajarwahyugumelar@gmail.com>
2022-06-13 12:12:16 +0700

    feat: Edit file index.route.js

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,13 +1,16 @@
 const indexController = require('../controllers/index.controller');
 const postController = require('../controllers/post.controller');
 const auth = require('../middlewares/auth');
-//pushcomment
+
 const router = require('express').Router();
 
 router.get('/', indexController.index);
 
-router.get('/post', postController.index);
-router.get('/post/create', postController.create);
-router.post('/post/create', postController.store);
+router.get('/posts', postController.index);
+router.get('/posts/create', postController.create);
+router.post('/posts/store', postController.store);
+router.get('/posts/edit/:id', postController.edit);
+router.post('/posts/update/:id', postController.update);
+router.post('/posts/delete/:id', postController.destroy);
 
 module.exports = router;
