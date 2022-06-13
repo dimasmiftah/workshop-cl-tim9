af089e6f49255dcc801a21e1ffa97b26adc4082b
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2022-06-13 11:17:47 +0700

    fix: remove update and delete

--- routes/index.route.js
+++ routes/index.route.js
@@ -9,8 +9,5 @@ router.get('/', indexController.index);
 router.get('/post', postController.index);
 router.get('/post/create', postController.create);
 router.post('/post/create', postController.store);
-router.post('/post/:id/edit', postController.update);
-router.get('/post/:id/delete', postController.delete);
-router.get('/post/:id/edit', postController.edit);
 
 module.exports = router;
