208cee43d63fce1875f972d502f2e8745129778d
mjafarshidik <mjafarshidik86@gmail.com>
2022-06-13 11:29:08 +0700

    feat: add edit, update and destroy

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -3,7 +3,7 @@ const Post = require('../models/Post');
 module.exports = {
   index: async (req, res) => {
     const posts = await Post.findAll();
-    //menambahkan komentar
+    
     return res.render('post/index', {
       posts,
     });
@@ -19,6 +19,45 @@ module.exports = {
       content: req.body.content,
     });
 
-    return res.redirect('/post');
+    return res.redirect('/posts');
+  },
+
+  edit: async (req, res) => {
+    const post = await Post.findByPk(req.params.id);
+
+    if (!post) {
+      return res.redirect('/posts');
+    }
+
+    return res.render('post/edit', {
+      post,
+    });
+  },
+
+  update: async (req, res) => {
+    const post = await Post.findByPk(req.params.id);
+
+    if (!post) {
+      return res.redirect('/posts');
+    }
+
+    await post.update({
+      title: req.body.title,
+      content: req.body.content,
+    });
+
+    return res.redirect('/posts');
+  },
+
+  destroy: async (req, res) => {
+    const post = await Post.findByPk(req.params.id);
+
+    if (!post) {
+      return res.redirect('/posts');
+    }
+
+    await post.destroy();
+
+    return res.redirect('/posts');
   },
 };
